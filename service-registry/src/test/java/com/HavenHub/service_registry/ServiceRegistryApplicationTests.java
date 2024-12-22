416bf3758d2b64f22f87feba74620d3ca80bc09d
samabishek42 <144218625+samabishek42@users.noreply.github.com>
2024-12-22 17:42:42 +0530

    Add files via upload
    adding service-registery files

---
+++ service-registry/src/test/java/com/HavenHub/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HavenHub.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
